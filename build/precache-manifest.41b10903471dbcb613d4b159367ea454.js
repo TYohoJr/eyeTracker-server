d92e0435dc4df85449245c6a97d85b564b023fa6
Thomas Yoho <TYohoJr@Gmail.com>
2019-02-23 15:04:24 -0600

    added basic server and build folder

---
+++ build/precache-manifest.41b10903471dbcb613d4b159367ea454.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a02881c454ec21ff35235ec9ef19a57",
+    "url": "/static/media/textured-background.6a02881c.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "fcb4e065e6c9c0f7ebcf",
+    "url": "/static/js/main.fcb4e065.chunk.js"
+  },
+  {
+    "revision": "cd74544378af0a30f684",
+    "url": "/static/js/2.cd745443.chunk.js"
+  },
+  {
+    "revision": "fcb4e065e6c9c0f7ebcf",
+    "url": "/static/css/main.1f399a11.chunk.css"
+  },
+  {
+    "revision": "2d640d45014db545ed882419cb75a369",
+    "url": "/index.html"
+  }
+];
---
+++ express.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+var path = require('path')
+require('dotenv').config();
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 8080, () => {
+    var curPort = process.env.PORT;
+    if (curPort === undefined) {
+        console.log(`listening on localhost://8080`)
+        curPort = "localhost://8080"
+    } else {
+        let d = new Date();
+        console.log(`listening on ${curPort} at ${d}`)
+    }
+})
